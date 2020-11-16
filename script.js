603d32da646ef861bb5b809e1bf7381d6c59f055
Vinicius Benedito <beneditoloura@gmail.com>
2020-11-16 07:16:33 -0300

    Drag

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
+/**
+ * Help
+ */
 function log(message) {
     console.log('> ' + message);
 }
 
-const cards = document.querySelectorAll('.card')
+const cards = document.querySelectorAll('.card');
+const dropzones = document.querySelectorAll('.dropzone');
 
 cards.forEach(card => {
     card.addEventListener('dragstart', dragstart);
@@ -11,14 +15,39 @@ cards.forEach(card => {
 });
 
 function dragstart() {
-    log('CARD: Start drag');
+    // log('CARD: Start drag');
 }
 
 function drag() {
-    log('> CARD: Is dragging');
+    // log('> CARD: Is dragging');
 
 }
 
 function dragend() {
-    log('> CARD: Stop drag');
+    // log('> CARD: Stop drag');
+}
+
+/* Local onde vou soltar os cartões */
+
+dropzones.forEach(dropzone => {
+    dropzone.addEventListener('dragenter', dragenter);
+    dropzone.addEventListener('dragover', dragover);
+    dropzone.addEventListener('dragleave', dragleave);
+    dropzone.addEventListener('drop', drop);
+});
+
+function dragenter() {
+    log('DROPZONE: Enter in zone');
+}
+
+function dragover() {
+    log('DROPZONE: Over');
+}
+
+function dragleave() {
+    log('DROPZONE: Leave');
+}
+
+function drop() {
+    log('DROPZONE: Dropped');
 }
